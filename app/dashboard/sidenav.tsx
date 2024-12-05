ac94b029a681960f966f46a4163d521d163d9839
aadi-netizen <kumaraditya325@gmail.com>
2024-12-05 17:25:19 +0530

    app routing, font and image optimization, Styling and initial set up

--- app/dashboard/sidenav.tsx
+++ app/dashboard/sidenav.tsx
@@ -1,7 +1,7 @@
-import Link from 'next/link';
-import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
-import { PowerIcon } from '@heroicons/react/24/outline';
+import Link from "next/link";
+import NavLinks from "@/app/dashboard/nav-links";
+import AcmeLogo from "@/app/ui/acme-logo";
+import { PowerIcon } from "@heroicons/react/24/outline";
 
 export default function SideNav() {
   return (
@@ -11,6 +11,7 @@ export default function SideNav() {
         href="/"
       >
         <div className="w-32 text-white md:w-40">
+          yf
           <AcmeLogo />
         </div>
       </Link>
